8524b4622cfd1fbecb6a71c8a179e8f2f6f43dd5
Veith M. Bürgerhoff <veith.buergerhoff@gmail.com>
2022-09-20 18:16:44 +0200

    removed tray icon and tiny refactor
    I found no use for the tray icon as there is no point in keeping the program running - just having it run once per downscale is enough and I don't need to provide different inputs. This also paves the way for better cross-plattform compatibility

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,12 @@ use image::{
     imageops::{resize, FilterType},
     Rgba, RgbaImage,
 };
-use {std::sync::mpsc, tray_item::TrayItem};
-
-enum Message {
-    Quit,
-}
 
 fn main() {
-    let mut tray = TrayItem::new("Clipboard Downscaler", "main-icon").unwrap();
-
-    tray.add_menu_item("Downscale", || {
-        downscale_image();
-    })
-    .unwrap();
-
-    let (tx, rx) = mpsc::channel();
-
-    tray.add_menu_item("Quit", move || {
-        tx.send(Message::Quit).unwrap();
-    })
-    .unwrap();
-
-    loop {
-        match rx.recv() {
-            Ok(Message::Quit) => break,
-            _ => {}
-        }
-    }
+    downscale_image(0.4);
 }
 
-fn downscale_image() {
+fn downscale_image(scaling_factor: f32) {
     let mut clipboard = Clipboard::new().unwrap();
     let clipboard_image = clipboard.get_image();
 
@@ -42,23 +18,25 @@ fn downscale_image() {
         Ok(image) => {
             let width = image.width.try_into().unwrap();
             let height = image.height.try_into().unwrap();
-            let mut rgba = to_rgba(&image.bytes, width, height);
-            let downscaled = resize(&mut rgba, width / 4, height / 4, FilterType::Lanczos3);
-            clipboard
-                .set_image(to_clipboard_image(downscaled, width / 4, height / 4))
-                .unwrap();
+
+            let rgba = to_rgba(&image.bytes, width, height);
+
+
+            let new_width = (image.width as f32 * scaling_factor) as u32;
+            let new_height = (image.height as f32 * scaling_factor) as u32;
+            let downscaled = resize(&rgba, new_width, new_height, FilterType::Lanczos3);
+
+            let new_clipboard_image = ImageData {
+                width: new_width as usize,
+                height: new_height as usize,
+                bytes: Cow::from(downscaled.into_raw()),
+            };
+            clipboard.set_image(new_clipboard_image).unwrap();
+            println!("finished downscaling");
         }
         Err(error) => {
             println!("something went wrong: {}", error);
         }
-    }
-}
-
-fn to_clipboard_image<'a>(rgba: RgbaImage, width: u32, height: u32) -> ImageData<'a> {
-    ImageData {
-        width: width as usize,
-        height: height as usize,
-        bytes: Cow::from(rgba.into_raw()),
     }
 }
 
